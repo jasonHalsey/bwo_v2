50174f8faaf6116f00f59fad28c65dcd69af23c5
JASON W HALSEY <jasonwhalsey@JASONs-MacBook-Pro.local>
2017-02-27 15:55:08 -0800

    Quick cleanup of app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 jQuery(document).foundation();
 
 $(window).on("load", function() {
-
-pageContentOffset();
-
+  pageContentOffset();
 });
 
 jQuery(document).ready(function() {
@@ -82,7 +80,6 @@ jQuery(window).scroll(function() {
 function pageContentOffset() {
   var heroheight = jQuery(".fixed_img_container > img").height();
   var pushheight = (heroheight / 2) - 70;
-  // jQuery('.river_title h1').css('margin-top',pushheight );
   jQuery('.river_title h1').css({
     marginTop : pushheight,
     transition : 'margin-top 1s ease-in-out'
